af928384253659b9bef1f51d0cccbcf36afab7ea
Chidozie Ononiwu <chononiw@microsoft.com>
2025-07-14 20:55:58 -0700

    Rename post-install-script, update CHANGELOG.md

--- eng/npm/wrapper/scripts/post-install-script.js
+++ eng/npm/wrapper/scripts/post-install-script.js
@@ -7,7 +7,6 @@ const requiredPackage = `@azure/mcp-${platform}-${arch}`;
 
 try {
   require.resolve(requiredPackage);
-  console.error(`Required Package '${requiredPackage}' Installed Succesfully`);
 } catch (err) {
   console.error(`Missing required package: '${requiredPackage}'`);
   process.exit(1);
